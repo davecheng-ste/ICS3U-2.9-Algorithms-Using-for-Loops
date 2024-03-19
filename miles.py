bf015c875dbae4160e855b8fcadf72a7966c5369
Dave Cheng <dave.cheng@ycdsb.ca>
2024-03-19 09:25:35 -0400

    Initial commit

---
+++ miles.py
@@ -0,0 +1,7 @@
+"""
+File: miles.py
+Author: Your Name
+Date: 2024-03-19
+Description: A program that outputs values for kilometres and equivalent miles 
+             every 10 km up to 100 km.
+"""
---
+++ sum.py
@@ -0,0 +1,7 @@
+"""
+File: sum.py
+Author: Your Name
+Date: 2024-03-19
+Description: A program that asks for user input, n, and calculates the
+             sum of numbers from 1 to n using an accumulator variable.
+"""
